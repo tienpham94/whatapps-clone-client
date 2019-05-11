fe0d576858173deb8316c6f1ac35ace418a5e780
Tien Pham <tiendev94@gmail.com>
2019-05-11 20:15:21 +0300

    Add ChatsListScreen

---
+++ src/components/ChatsListScreen/index.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { Suspense } from "react";
+
+import Navbar from "../Navbar";
+import ChatsList from "./ChatsList";
+import ChatsNavbar from "./ChatsNavbar";
+
+export default () => (
+  <div className="ChatsListScreen Screen">
+    <Navbar>
+      <ChatsNavbar />
+    </Navbar>
+    <Suspense fallback={null}>
+      <ChatsList />
+    </Suspense>
+  </div>
+);
